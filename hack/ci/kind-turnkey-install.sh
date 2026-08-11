fa5ebe52adaa9bb0e9a2564b7853cdba85c6ee14
Romans Pokrovskis <motivated.it@gmail.com>
2026-08-11 05:27:55 +0300

    test: pin non-root DNS canary identity

--- hack/ci/kind-turnkey-install.sh
+++ hack/ci/kind-turnkey-install.sh
@@ -624,6 +624,8 @@ spec:
         capabilities:
           drop: ["ALL"]
         runAsNonRoot: true
+        runAsUser: 1000
+        runAsGroup: 1000
         seccompProfile:
           type: RuntimeDefault
 EOF
